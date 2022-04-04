e565f75180359a5254ea65c123ae641f02ea9295
belenbarrionuevo <belenpotf96@gmail.com>
2022-04-04 19:42:45 -0300

    Nuevas funciones a los anillos de habilidades
    • Se agrega la funcionalidad de que muestren en el centro el nombre de la habilidad a evaluar y un número porcentual de manera dinámica.
    • Se crea la lógica para que responda a datos cargados en un objeto de manera dinámica.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let progressValue = document.querySelector('.skillPercentageValue');
+let progressColor = document.querySelector('.progressColor');
+let progressTitle = document.querySelector('.skillPercentageTitle');
+let counter = 0;
+const progressData = { "skill": "Comunicaciones", "value": 100 };
+
+// En esta sección se le dará funcionalidad a los medidores de habilidades de la página principal.
+
+const skillValue = progressData.value;
+
+function percentage(skillValue) {
+    return 680 / 100 * skillValue;
+}
+
+console.log(percentage(skillValue));
+
+progressColor.style.cssText = `stroke-dasharray: ${percentage(skillValue) + 2000};`;
+
+progressTitle.innerHTML = progressData.skill;
+
+setInterval(() => {
+    progressValue.innerHTML = counter + "%";
+    if (counter === progressData.value) {
+        clearInterval();
+    } else {
+        counter += 1;
+    }
+}, 15.7);
